93978ae61d593669039483b4180e60f15f2324d5
godakid <godakid@outlook.com>
2024-08-07 22:06:50 +0800

    feat: 对接K线数据，调整部分侧边布局

--- app/(clientLayout)/home/layout.tsx
+++ app/(clientLayout)/home/layout.tsx
@@ -4,36 +4,81 @@ import { Split } from "@gfazioli/mantine-split-pane";
 import {
   Flex,
   Group,
-  List,
-  Popover,
+  Input,
+  Menu,
   Stack,
   Text,
   UnstyledButton
 } from "@mantine/core";
 import { KlineChartModule } from "@/app/components/KlineCharts/core";
 import { executeCommand } from "@/app/hooks/use-event-emitter";
-import { type PropsWithChildren, useEffect } from "react";
+import { type PropsWithChildren, useEffect, useRef } from "react";
 import { ContextMenuTrigger } from "rctx-contextmenu";
 import { ContextMenuEnum } from "@/app/components/ui/ContextMenu/types";
-import { stockMarketKlineChartAtom } from "@/app/utils/store/chartStore";
-import { useAtom } from "jotai";
+import {
+  curSearchStockKeywordAtom,
+  curSelectedStockAtom,
+  stockMarketKlineChartAtom
+} from "@/app/utils/store/chartStore";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import Loading from "@/app/components/base/loading";
+import { getHotkeyHandler } from "@mantine/hooks";
 
-export default function KLineChartLayout({ children }: PropsWithChildren) {
+function SearchStockInput() {
+  const updateCurSearchStockKeywordAtom = useSetAtom(curSearchStockKeywordAtom);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleEnterSearch = getHotkeyHandler([
+    [
+      "Enter",
+      (ev) => {
+        // 聚焦搜索
+        updateCurSearchStockKeywordAtom(ev.target.value);
+      }
+    ]
+  ]);
+  return (
+    <Input
+      ref={inputRef}
+      placeholder={"搜索"}
+      className={"!h-full"}
+      onKeyDown={handleEnterSearch}
+      rightSection={<i className={"i-mdi-search"} />}
+      size="xs"
+    />
+  );
+}
+function AsideHeader() {
+  return (
+    <Group justify={"space-between"} py={"4px"} className={"box-border"}>
+      <Menu>
+        <Menu.Target>
+          <UnstyledButton className={"flex items-center"}>
+            <span className="i-[material-symbols-light--expand-circle-down-outline]"></span>
+            <Text size={"sm"}>自选组</Text>
+          </UnstyledButton>
+        </Menu.Target>
+        <Menu.Dropdown p={"xs"}>
+          <Menu.Item component="a" href="https://mantine.dev">
+            股票列表
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+
+      <Group mr={"12px"}>
+        <SearchStockInput />
+      </Group>
+    </Group>
+  );
+}
+
+function KLineChart() {
   const klineChartMemo = KlineChartModule();
   const { init } = klineChartMemo;
   const { ref: klineRef } = init();
   const [{ data: stockMarketKlineChartData, isLoading }] = useAtom(
     stockMarketKlineChartAtom
   );
-
-  useEffect(() => {
-    if (!stockMarketKlineChartData?.content) return;
-    klineChartMemo.chart.applyNewData?.(
-      stockMarketKlineChartData.content ?? []
-    );
-  }, [stockMarketKlineChartData?.content]);
-
+  const curSelectedStockCode = useAtomValue(curSelectedStockAtom);
   useEffect(() => {
     const rsi = klineChartMemo.chart.createIndicator("RSI", true);
     const boll = klineChartMemo.chart.createIndicator("BOLL", true);
@@ -42,6 +87,37 @@ export default function KLineChartLayout({ children }: PropsWithChildren) {
       klineChartMemo.chart.removeIndicator(boll!);
     };
   }, [klineChartMemo.chart]);
+  useEffect(() => {
+    if (!stockMarketKlineChartData?.content) return;
+    klineChartMemo.chart.applyNewData?.(
+      stockMarketKlineChartData.content ?? []
+    );
+  }, [stockMarketKlineChartData?.content]);
+  return (
+    <div className={"relative w-full grow"} ref={klineRef}>
+      <div
+        className={
+          "pointer-events-none absolute left-[50%] top-[50%] z-[1] flex size-full translate-x-[-50%] translate-y-[-50%] items-center justify-center"
+        }
+      >
+        <Text c="gray" opacity={0.05} size={"10vw"}>
+          {curSelectedStockCode}
+        </Text>
+      </div>
+      {isLoading && (
+        <div
+          className={
+            "bg-[#000]] pointer-events-none absolute left-[50%] top-[50%] z-[100] flex size-full translate-x-[-50%] translate-y-[-50%] items-center backdrop-blur"
+          }
+        >
+          <Loading />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function KLineChartLayout({ children }: PropsWithChildren) {
   return (
     <Split size="xs" h={"100vh"}>
       <Split.Pane
@@ -52,20 +128,11 @@ export default function KLineChartLayout({ children }: PropsWithChildren) {
         }}
       >
         <Stack className={"h-100vh flex w-full flex-col"}>
-          {isLoading && (
-            <div
-              className={
-                "bg-[#000]] pointer-events-none absolute left-[50%] top-[50%] z-[100] flex size-full translate-x-[-50%] translate-y-[-50%] items-center backdrop-blur"
-              }
-            >
-              <Loading />
-            </div>
-          )}
           <ContextMenuTrigger
             id={ContextMenuEnum.CHART}
             className={"relative flex w-full grow"}
           >
-            <div className={"w-full grow"} ref={klineRef}></div>
+            <KLineChart></KLineChart>
           </ContextMenuTrigger>
         </Stack>
       </Split.Pane>
@@ -77,51 +144,7 @@ export default function KLineChartLayout({ children }: PropsWithChildren) {
         key={"right"}
       >
         <Flex w={"100%"} p={"4px"} direction={"column"} gap={6}>
-          <Group justify={"space-between"}>
-            <Popover>
-              <Popover.Target>
-                <UnstyledButton className={"flex items-center"}>
-                  <span className="i-[material-symbols-light--expand-circle-down-outline]"></span>
-                  <Text size={"sm"}>自选组</Text>
-                </UnstyledButton>
-              </Popover.Target>
-              <Popover.Dropdown p={0} bg={"transparent"} maw={200}>
-                <List
-                  p={0}
-                  bg={"var(--mantine-color-body)"}
-                  className={"dark:bg-[#191919]!"}
-                >
-                  <List.Item
-                    className={
-                      "cursor-pointer px-[4px] py-[6px] hover:hover:bg-[var(--mantine-color-default-hover)]"
-                    }
-                  >
-                    <Text
-                      size={"sm"}
-                      className={"flex items-center justify-center"}
-                    >
-                      <i className={"i-mdi-eye"}></i>
-                      总览列表
-                    </Text>
-                  </List.Item>
-
-                  <List.Item
-                    className={
-                      "cursor-pointer px-[4px] py-[6px] hover:hover:bg-[var(--mantine-color-default-hover)]"
-                    }
-                  >
-                    <Text
-                      size={"sm"}
-                      className={"flex items-center justify-center"}
-                    >
-                      <i className={"i-mdi-add-circle"}></i>添加分组
-                    </Text>
-                  </List.Item>
-                </List>
-              </Popover.Dropdown>
-            </Popover>
-          </Group>
-
+          <AsideHeader />
           {children}
         </Flex>
       </Split.Pane>
--- app/components/page/home/HomeNav.tsx
+++ app/components/page/home/HomeNav.tsx
@@ -1,6 +1,13 @@
 "use client";
 import "react-data-grid/lib/styles.css";
-import { AppShell, Group, ThemeIcon, Tooltip } from "@mantine/core";
+import {
+  AppShell,
+  Avatar,
+  rem,
+  ScrollArea,
+  ThemeIcon,
+  Tooltip
+} from "@mantine/core";
 import useBreakpoints, { MediaType } from "@/app/hooks/use-breakpoints";
 import { Logo } from "@/app/components/ui/Logo";
 import { cx } from "@emotion/css";
@@ -31,39 +38,43 @@ export default function HomeNav() {
     <>
       {mediaType !== MediaType.mobile ? (
         <AppShell.Navbar
-          px="md"
+          px={rem(8)}
           py="sm"
           className={"overflow-hidden"}
           zIndex={2}
         >
+          <AppShell.Section
+            className={"flex items-center justify-between"}
+            mx={"auto"}
+          >
+            <div
+              className={cx(
+                `border-gray dark:text-blue mb-[12px] box-border flex size-[30px] items-center justify-center rounded-md border-[1px] border-solid p-[2px]`
+              )}
+            >
+              <Logo />
+            </div>
+          </AppShell.Section>
+
+          <AppShell.Section grow mx={"auto"} component={ScrollArea}>
+            {rightTools.map((item) => {
+              return (
+                <Tooltip key={item.key} label={item.description}>
+                  <ThemeIcon
+                    variant="gradient"
+                    gradient={{ from: "orange", to: "red", deg: 282 }}
+                    radius={"sm"}
+                    className={cx("cursor-pointer")}
+                    onClick={() => item.executor?.()}
+                  >
+                    <i className={cx(item.icon)}></i>
+                  </ThemeIcon>
+                </Tooltip>
+              );
+            })}
+          </AppShell.Section>
           <AppShell.Section>
-            <Group>
-              <div
-                className={cx(
-                  `border-gray dark:text-blue box-border size-[30px] rounded-md border-[1px] border-solid p-[2px]`
-                )}
-              >
-                <Logo />
-              </div>
-              <AppShell.Section>
-                {rightTools.map((item) => {
-                  return (
-                    <Tooltip key={item.key} label={item.description}>
-                      <ThemeIcon
-                        variant="gradient"
-                        size="md"
-                        gradient={{ from: "orange", to: "red", deg: 282 }}
-                        radius={"sm"}
-                        className={cx("cursor-pointer")}
-                        onClick={() => item.executor?.()}
-                      >
-                        <i className={cx(item.icon)}></i>
-                      </ThemeIcon>
-                    </Tooltip>
-                  );
-                })}
-              </AppShell.Section>
-            </Group>
+            <Avatar component="button">A</Avatar>
           </AppShell.Section>
         </AppShell.Navbar>
       ) : null}
